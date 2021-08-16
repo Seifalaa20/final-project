bd542cdcf14580a0a1c0860f5137d47a3c53bf36
Seifalaa20 <Seifalaa36@gmail.com>
2021-08-16 02:43:20 +0200

    Update final team10.py

--- final team10.py
+++ final team10.py
@@ -82,3 +82,67 @@ mouse = pygame.image.load("mouse.png").convert_alpha()
 mouse = pygame.transform.scale(mouse, (60, 60))
 cat = pygame.image.load("cat.png").convert_alpha()
 cat = pygame.transform.scale(cat, (60, 60))
+LEFT_presses = 0
+right_presses = 0
+UP_presses = 0
+DOWN_presses = 0
+WIDTH = 66
+HEIGHT = 66
+MARGIN = 1
+grid = []
+seif = [[66, 0], [-66, 0], [0, -66], [0, 66]]
+way = random.randint(0, 1)                             # >>>>>>>>>>>><<<<<<<<<<<<
+
+for row in range(10):                           # Drawing grid
+    grid.append([])
+    for column in range(10):
+        grid[row].append(0)
+grid[1][5] = 1
+
+pygame.init()                        # Initializing
+
+WINDOW_SIZE = [600, 600]                        # Window setting
+screen = pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption("Mouse Escape")
+done = False
+window = True
+clock = pygame.time.Clock()
+def text_objects(text, font):      # Font and size function
+    textsurface = font.render(text, True, RED)
+    return textsurface, textsurface.get_rect()
+
+
+def message_display(text):         # Message function
+    largetext = pygame.font.Font('freesansbold.ttf', 50)
+    textsurf, textrect = text_objects(text, largetext)
+    textrect.center = ((600 / 2), (600 / 2))
+    screen.blit(textsurf, textrect)
+    pygame.display.update()
+
+
+def starve():         # Message when starving
+    message_display('You starved to Death')
+
+
+def escape():         # Message when escape
+    message_display('You escaped')
+
+
+def water():          # Message when drowned
+    message_display("you drowned")
+
+
+def dead():            # Message when die
+    message_display("you are dead")
+
+
+def movement():        # Mouse moving function
+    i, j = random.choice(seif)
+    global current_pos, mouse_posy, mouse_posx
+    mouse_posy += j
+    mouse_posx += i
+    current_pos = (mouse_posx, mouse_posy)
+
+
+def keys():
+    message_display("Move the mouse using the arrows")
